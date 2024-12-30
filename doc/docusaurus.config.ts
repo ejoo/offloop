3fa2ea43870376e9301b77d33f69c0c09c779187
alihussain575 <alihussainirfan575@gmail.com>
2024-12-30 16:31:48 +0500

    Add GitHub Actions workflow for deployment

--- doc/docusaurus.config.ts
+++ doc/docusaurus.config.ts
@@ -10,10 +10,12 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://ejoo.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/offloop/',
+  organizationName : 'uconnect',
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
