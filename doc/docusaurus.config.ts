7cc06b5e0a2ad79b3437c8ce9e624bf96a9fe1dc
alihussain575 <alihussainirfan575@gmail.com>
2024-12-30 17:40:03 +0500

    changes

--- doc/docusaurus.config.ts
+++ doc/docusaurus.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/offloop/',
-  organizationName : 'uconnect',
+  organizationName : 'ejoo',
   trailingSlash: false,
 
   // GitHub pages deployment config.
